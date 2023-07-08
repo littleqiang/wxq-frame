92dd527d32a08a7bd8c6fd1afb193d2609e4ffdc
littleqiang <wangxuqiang1988@gmail.com>
2023-07-08 12:53:17 +0800

    初始化

---
+++ codemanage/src/main/java/com/wxq/codemanage/CodemanageApplication.java
@@ -0,0 +1,13 @@
+package com.wxq.codemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodemanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodemanageApplication.class, args);
+	}
+
+}
